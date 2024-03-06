de5508ddd76f6679076d70a127943e1ca09b3501
vignesh <selvasitpl@gmail.com>
2024-03-06 12:15:35 +0530

    first commit

---
+++ src/app/MyMapComponent.js
@@ -0,0 +1,82 @@
+import { useEffect, useRef, useState, memo } from "react";
+import styles from "./MyMapComponent.css";
+
+function getLocations() {
+  const locationsList = [
+    [{name: "Nivesh", lat: 11.0168, lng: 76.9558}, {name: "Rajesh", lat: 10.0168, lng: 77.9558}],
+    [{name: "Nivesh", lat: 12.0168, lng: 76.9558}, {name: "Rajesh", lat: 10.0168, lng: 75.9558}],
+    [{name: "Nivesh", lat: 11.0168, lng: 73.9558}, {name: "Rajesh", lat: 10.0168, lng: 73.9558}],
+    [{name: "Nivesh", lat: 11.0168, lng: 74.9558}, {name: "Rajesh", lat: 10.0168, lng: 75.9558}],
+  ];
+  return locationsList[Math.floor(Math.random() * locationsList.length)];
+}
+
+function clearMarkers(markers) {
+  markers.forEach(marker => marker.setMap(null));
+  markers.length = 0;
+}
+
+function _addMarker(markers, map, position) {
+  const marker = new window.google.maps.Marker({ map, position });
+  markers.push(marker);
+  return marker;
+}
+
+function addMarker(markers, map, position) {
+  _addMarker(markers, map, position);
+  map.panTo(position);
+}
+
+function addMarkers(markers, map, locations) {
+  locations.forEach(location => {
+    const marker = _addMarker(markers, map, location);
+    const infoWindow = new window.google.maps.InfoWindow({content: location.name});
+    marker.addListener("click", () => infoWindow.open({anchor: marker, map}));
+    infoWindow.open({anchor: marker, map});
+  });
+  const bounds = new window.google.maps.LatLngBounds();
+  locations?.forEach(({ lat, lng }) => bounds.extend({ lat, lng }));
+  map.fitBounds(bounds);
+}
+
+function getMap(ref) {
+  const zoom = 11;
+  return new window.google.maps.Map(ref.current, {
+    zoom,
+  });
+}
+
+export const MyMapComponent = memo(function MyMapComponent() {
+  const ref = useRef();
+  const map = useRef();
+  const markers = useRef([]);
+
+  const [locations, setLocations] = useState(getLocations());
+
+  useEffect(() => {
+    console.log("adding fetching locations call back.");
+    const watchId = setInterval(() => {
+      console.log("setting locations.");
+      setLocations(getLocations());
+    },
+      20000);
+    return () => {
+      console.log("clearing fetching locations callback.");
+      clearInterval(watchId);
+    };
+  });
+
+  useEffect(() => {
+    if (map.current == null) {
+      console.log("map generated.");
+      map.current = getMap(ref);
+    }
+
+    console.log("Updating markers.");
+    clearMarkers(markers.current);
+    addMarkers(markers.current, map.current, locations);
+  }, [locations]);
+
+  console.log("rendering map.");
+  return <div ref={ref} id="map" />;
+});
--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,50 @@
+"use client"
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useEffect, useState } from "react";
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
+import { MyMapComponent } from "./MyMapComponent";
+
+const render = (status) => {
+  if (status === Status.LOADING) return <h3>{status} ..</h3>;
+  if (status === Status.FAILURE) return <h3>{status} ...</h3>;
+  return null;
+};
 
 export default function Home() {
+  const [locationSharingEnabled, enableLocationSharing] = useState(false);
+  useEffect(() => {
+    var watchId = null;
+    if (locationSharingEnabled) {
+      console.log("adding watch position callback.");
+      watchId = navigator.geolocation.watchPosition(({ coords: { latitude: lat, longitude: lng } }) => {
+        console.log("watching position: " + (lat + " | " + lng));
+      },
+      () => { },
+      {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+      });
+    }
+    return () => {
+      if (watchId != null) {
+        console.log("clearing watch position callback.");
+        navigator.geolocation.clearWatch(watchId);
+      }
+    }
+  }, [locationSharingEnabled]);
+  console.log("rendering app.");
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div>
+      <label>
+        <input type="checkbox" checked={locationSharingEnabled} onChange={() => enableLocationSharing(!locationSharingEnabled)} />
+        Share Location
+      </label>
+      <Wrapper apiKey={process.env.NEXT_PUBLIC_API_KEY} render={render}>
+        <MyMapComponent />
+      </Wrapper>
+    </div>
   );
 }
